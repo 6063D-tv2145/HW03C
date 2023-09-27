25813e404eba4b670c14ea568464bd9d9bf6763d
Thao Chi Vu <tv2145@nyu.edu>
2023-09-27 13:28:58 -0400

    attempt at solution -- circles randomized, lines

--- sketch.js
+++ sketch.js
@@ -1,6 +1,24 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+let CircleCount = 24;
+let LineCount = 12;
 
+function setup() {
+  createCanvas(800, 800);
+}
 function draw() {
+  randomSeed(1000)
+  background(161, 193,255 );
+
+  for (let c = 0; c <= CircleCount; c+=1) {
+    let x1 = random(width);
+    let y1 = random(height);
+    strokeWeight(0)
+    ellipse(x1, y1, 30, 30);
+    
+    for (let l = c; l <= LineCount; l+=1) {
+      let x2 = (CircleCount-1)*x1;
+      let y2 = (CircleCount-1)*y1;
+      strokeWeight(1)
+      line(x1, y1, x2, y2);
+    }
+  }
 }
